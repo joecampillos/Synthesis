169bec508a7a572e5f574f63b044867978a74225
Jonathan <joecampillos@gmail.com>
2020-02-19 16:59:17 +0800

    fix

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper('.client-slide', {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    setWrapperSize: true,
+    slidesOffsetBefore: 5,
+    watchOverflow: true,
+    centeredSlides: true,
+    loop: true,
+    // init: false,
+    autoplay: {
+        delay: 3000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 25,
+      },
+    }
+  });
